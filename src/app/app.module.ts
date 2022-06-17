d2157e64e1fe8126a0a577eb06e83a37fd81f322
theinsideshine <educacion.ta@gmail.com>
2022-06-17 07:32:15 -0300

    Se conf router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { AppComponent } from './app.component';
 import { ProyectsComponent } from './proyects/proyects.component';
 import { ProyectService } from './proyects/proyect.service';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/proyects', pathMatch: 'full'},
+  {path: 'proyects', component: ProyectsComponent},
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,9 @@ import { ProyectService } from './proyects/proyect.service';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [ ProyectService ],
   bootstrap: [AppComponent]
